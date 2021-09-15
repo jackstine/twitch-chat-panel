13b27419ad3fa83d3a094d8ffd7fb50b5e1af57c
Jake Cukjati <jakecukjati@gmail.com>
2021-09-14 21:44:17 -0400

    added in a default giphy

--- src/components/GiphyBox.jsx
+++ src/components/GiphyBox.jsx
@@ -9,8 +9,9 @@ import TwitchBotAPI from '../apis/TwitchBotAPI'
  * if nothing is returned, change the timelimit to a new time
  */
 
-let MAX_TIME = 1000 * 60 * 3
+let MAX_TIME = 1000 * 60 * 5
 let INTERVAL_TIME = 1000 * 5
+let DEFAULT_GIPHY = process.env.REACT_APP_GIPHY
 
 const GiphyBox = function () {
   const getterFunc =  useCallback((time) => {
@@ -24,26 +25,33 @@ const GiphyBox = function () {
   }, [])
   const [giph, setGiph] = useState(null)
   const [timelimit, setTimelimit] = useState(INTERVAL_TIME)
+  const [defaultGiphy, setDefaultGiphy] = useState(DEFAULT_GIPHY)
   useEffect(() => {
     getterFunc(timelimit).then(giph => {
       if (giph.data) {
-        setTimelimit(MAX_TIME)
+        if (timelimit === MAX_TIME) {
+          setTimelimit(MAX_TIME + 10)
+        } else {
+          setTimelimit(MAX_TIME)
+        }
+        setDefaultGiphy('')
       } else {
         if (timelimit === INTERVAL_TIME) {
-          setTimelimit(INTERVAL_TIME + 1)
+          setTimelimit(INTERVAL_TIME + 10)
         } else {
           setTimelimit(INTERVAL_TIME)
         }
-      } 
-      console.log(giph.data)
+        setDefaultGiphy(DEFAULT_GIPHY)
+      }
       setGiph(giph.data)
     })
   }, [timelimit])
-  let render = giph !== null
+  let selectedGiph = defaultGiphy !== '' ? defaultGiphy : giph?.message
+  let render = selectedGiph !== null && selectedGiph !== undefined
   return (render &&
     <div>
       {/* name */}
-      <Giphy large={true} data={giph.message}/>
+      <Giphy large={true} data={selectedGiph}/>
     </div>
   )
 }
--- src/components/ProfileBox.jsx
+++ src/components/ProfileBox.jsx
@@ -5,7 +5,7 @@ const ProfileBox = function (props) {
     'color': props.tags.color
   }
   let NewMessages = props.messages.map(m => <div key={m.id} className="chat-box-messages-message"><MessageChain {...m.messageChain}/></div>)
-  let badgeImages = props.badges.map(url => <img src={url} className="profile-badge"/>)
+  let badgeImages = props.badges ? props.badges.map(url => <img src={url} className="profile-badge"/>) : null
   return (
   <div className="profile-box">
       <div className="profile-box-username" style={usernameStyles}>
